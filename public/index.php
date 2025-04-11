f572e4c564b220b8ddd3bf0b148aca044f2a3266
Nikolay Zuev <n.zuev@webinar.ru>
2025-04-11 15:16:32 +0500

    upd

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-file_put_contents('php://stderr', '{"level":"info","channel":"app","datetime":"2025-04-09T10:43:54+03:00","msg":"eee"}');
+sleep(5);
+
+file_put_contents('php://stderr', '{"level":"info","channel":"app","datetime":"2025-04-09T10:43:54+03:00","msg":"yes"}');
 
 file_put_contents(__DIR__.DIRECTORY_SEPARATOR.date('Y-m-d').'_log.log', '['.date('H:i:s').']'.PHP_EOL.print_r(
 1
