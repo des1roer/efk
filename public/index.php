0a61f083b8d7aaaa85b50087db0d32f0550bc9cc
Nikolay Zuev <n.zuev@webinar.ru>
2025-04-11 15:09:32 +0500

    upd

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "123";
 file_put_contents('php://stderr', '{"level":"info","channel":"app","datetime":"2025-04-09T10:43:54+03:00","msg":"eee"}');
 
+file_put_contents(__DIR__.DIRECTORY_SEPARATOR.date('Y-m-d').'_log.log', '['.date('H:i:s').']'.PHP_EOL.print_r(
+1
+, true).PHP_EOL, FILE_APPEND | LOCK_EX);
